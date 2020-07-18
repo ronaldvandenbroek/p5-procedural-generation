d2f122db1835b4166f97eb7558acc13b07200432
Ronald van den Broek <ronald.vandenbroek@live.nl>
2020-07-18 21:00:11 +0200

    Adding generatePointField

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const resolution: number = 20;
 const seed: number = 1;
 
+
 function setup() {
   // windowWidth and windowHeigt are global p5 variables
   createCanvas(windowWidth, windowHeight);
@@ -20,15 +21,30 @@ function windowResized() {
 
 function drawGrid() {
   background(0)
-  strokeWeight(10);
+  strokeWeight(7);
 
   const colums = windowWidth / resolution + 1;
   const rows = windowHeight / resolution + 1;
 
+  const pointField: number[][] = generatePointField(colums, rows);
+
+  //Draw
   for (let i = 0; i < colums; i++) {
     for (let j = 0; j < rows; j++) {
-      stroke(floor(random(0, 255)));
+      stroke(pointField[i][j]);
       point(i * resolution, j * resolution);
     }
   }
+}
+
+function generatePointField(colums: number, rows: number): number[][] {
+  const pointField: number[][] = [];
+  for (let i = 0; i < colums; i++) {
+    pointField[i] = [];
+    for (let j = 0; j < rows; j++) {
+      const value = ceil(random(-1, 1)) * 255;
+      pointField[i][j] = value;
+    }
+  }
+  return pointField;
 }
